c8ffd18a1a15cd17a710d78a7d39afd7d2e8afd5
Mackenzie Moore <macmoore@ucdavis.edu>
2019-02-10 05:30:40 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,7 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(planeteer_calls)
-    string = ""
+   new_array = []
   planeteer_calls.each do |element|
     string += "#{element.capitalize}!"
 end
