79f23e5ab2b15a972aa97e4979a8a8903cceb876
Mackenzie Moore <macmoore@ucdavis.edu>
2019-02-10 05:31:14 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,7 @@ def summon_captain_planet(planeteer_calls)
   planeteer_calls.each do |element|
     new_array << "#{element.capitalize}!"
 end
-  puts new_array
+  returns new_array
 end
 
 def long_planeteer_calls# code an argument here
